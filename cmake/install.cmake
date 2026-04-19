2c1453d74c1aba96d502531baae0e94090ef9bc6
Florian Piesche <florian@yellowkeycard.net>
2026-04-19 19:43:12 +0100

    Add FreeDesktop metadata

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -13,23 +13,29 @@ install(FILES ${DIST_PATH}/rs.cfg
                 OWNER_READ OWNER_WRITE OWNER_EXECUTE
 )
 
-# if(UNIX AND NOT APPLE)
-#     install(FILES ${DIST_PATH}/${FREEDESKTOP_APP_ID}.svg
-#             DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/icons/hicolor/scalable/apps/
-#             PERMISSIONS WORLD_READ
-#                         GROUP_READ
-#                         OWNER_READ OWNER_WRITE OWNER_EXECUTE
-#     )
-#     install(FILES ${DIST_PATH}/linux/${FREEDESKTOP_APP_ID}.metainfo.xml
-#             DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/metainfo/
-#             PERMISSIONS WORLD_READ
-#                         GROUP_READ
-#                         OWNER_READ OWNER_WRITE OWNER_EXECUTE
-#     )
-#     install(FILES ${DIST_PATH}/linux/${FREEDESKTOP_APP_ID}.desktop
-#             DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/applications/
-#             PERMISSIONS WORLD_READ WORLD_EXECUTE
-#                         GROUP_READ GROUP_EXECUTE
-#                         OWNER_READ OWNER_WRITE OWNER_EXECUTE
-#     )
-# endif()
+if(UNIX AND NOT APPLE)
+    install(FILES ${DIST_PATH}/${FREEDESKTOP_APP_ID}.svg
+            DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/icons/hicolor/scalable/apps/
+            PERMISSIONS WORLD_READ
+                        GROUP_READ
+                        OWNER_READ OWNER_WRITE OWNER_EXECUTE
+    )
+    install(FILES ${DIST_PATH}/${FREEDESKTOP_APP_ID}.png
+            DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/icons/hicolor/256x256/apps/
+            PERMISSIONS WORLD_READ
+                        GROUP_READ
+                        OWNER_READ OWNER_WRITE OWNER_EXECUTE
+    )
+    install(FILES ${DIST_PATH}/linux/${FREEDESKTOP_APP_ID}.metainfo.xml
+            DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/metainfo/
+            PERMISSIONS WORLD_READ
+                        GROUP_READ
+                        OWNER_READ OWNER_WRITE OWNER_EXECUTE
+    )
+    install(FILES ${DIST_PATH}/linux/${FREEDESKTOP_APP_ID}.desktop
+            DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/applications/
+            PERMISSIONS WORLD_READ WORLD_EXECUTE
+                        GROUP_READ GROUP_EXECUTE
+                        OWNER_READ OWNER_WRITE OWNER_EXECUTE
+    )
+endif()
